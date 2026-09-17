8b7c2bb4535be67f95207c6e7e750e523f826eb1
juha0414 <juha3129@goedu.kr>
2026-09-17 10:28:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
 # --------------------------------------------------
 # 1. 기본 설정
 # --------------------------------------------------
